50cc08dbf8ca1b128f22606a65e7c5c153a08511
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2025-07-31 23:05:55 -0500

    Actualizacion

--- 13.Ejercicio/js/promedio.js
+++ 13.Ejercicio/js/promedio.js
@@ -2,8 +2,9 @@
 
 // Metodo para obtener el calculo del promedio
 const calcularPromedio = () => {
+    let cantNotas;
     do {
-        let cantNotas = prompt("Introduzca la cantidad de notas:");
+        cantNotas = prompt("Introduzca la cantidad de notas:");
         if(cantNotas === null){
             return;
         }
@@ -15,13 +16,15 @@ const calcularPromedio = () => {
     try {
         let contadorNotas = 0;
         for(let i = 0; i < contadorNotas; i ++){
+            let nota;
             do { 
-                let nota = promt(`Introduza la nota ${i + 1}:`); //
+                nota = promt(`Introduza la nota ${i + 1}:`); //
                 if(nota === null){
-                    return
+                    return;
                 }
-                if()
-            } while (condition);
+                if(isNaN(parseFloat(nota)));
+            } while (isNaN(parseFloat(nota)));
+            acumuladorNotas += parseFloat(nota);
         }
         let promedio = acumuladorNotas / cantNotas;
         alert("EL promedio es: " + promedio.toFixed(1));
